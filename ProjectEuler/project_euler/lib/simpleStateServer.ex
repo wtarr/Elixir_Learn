e471afe656e5a25b392e8c4f41ffa374222b889c
Will <william.projects.2012@gmail.com>
2015-02-06 21:12:37 +0000

    added simple state server

---
+++ ProjectEuler/project_euler/lib/simpleStateServer.ex
@@ -0,0 +1,33 @@
+defmodule StateServer do
+
+  def state(value) do
+    receive do
+      {sender, :update, newvalue} ->
+        state(newvalue)
+      {sender, :get} ->
+        send sender, {:ok, value}
+        state(value)
+      {sender, :exit} ->
+        IO.puts("exiting ...")
+    end
+  end
+
+  def start(initialValue) when is_integer(initialValue) do
+    spawn(__MODULE__, :state, [initialValue])
+  end
+
+  def get(pid) do
+    send pid, {self, :get}
+    receive do
+      {:ok, value} -> IO.puts "received #{value}"
+    after 1000 ->
+      IO.puts "received nothing"
+    end
+  end
+
+  def update(pid, value) when is_integer(value) do
+    send pid, {self, :update, value}
+    get(pid)
+  end
+
+end
