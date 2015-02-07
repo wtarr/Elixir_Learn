7a52dba708a7b42432cdb2785d803fad7de28b98
Will <william.projects.2012@gmail.com>
2015-02-07 23:44:23 +0000

    abstract repetitive -gt test to function

--- ProjectEuler/project_euler/lib/problem4.ex
+++ ProjectEuler/project_euler/lib/problem4.ex
@@ -1,25 +1,23 @@
 defmodule Problem4 do
+  def greater(a, b) do
+    if a > b do
+      a
+    else
+      b
+    end
+  end
+
   def solve() do
     Enum.reduce(100..999, 0, fn(x, outer) ->
       test = Enum.reduce(100..999, 0, fn(y, inner) ->
         candidate = x * y
         if HelperLibrary.is_palindrome(candidate) do
-          if candidate > inner do
-            candidate
-          else
-            inner
-          end
+          greater(candidate, inner)
         else
           inner
         end
       end)
-
-      if test > outer do
-        test
-      else
-        outer
-      end
-
+      greater(test, outer)
     end)
   end
 end
