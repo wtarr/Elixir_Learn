185f6e68f6e1e3e6f05f27c691ef16a0fd7569dd
Will <william.projects.2012@gmail.com>
2015-02-17 22:49:46 +0000

    file stream for reading, out lined steps for completion

--- ProjectEuler/project_euler/lib/helperLibrary.ex
+++ ProjectEuler/project_euler/lib/helperLibrary.ex
@@ -5,4 +5,10 @@ defmodule HelperLibrary do
     candidate_as_string == String.reverse(candidate_as_string)
   end
 
+  def process_file_to_list!(path, function) do
+    File.stream!(path)
+    |> Stream.map(&String.replace(&1, "\n", ""))
+    |> Enum.map(&function.(&1))
+  end
+
 end
--- ProjectEuler/project_euler/lib/problem17.ex
+++ ProjectEuler/project_euler/lib/problem17.ex
@@ -1,24 +1,26 @@
 defmodule Problem17 do
 
-  # def countLettersOfNumberAsWord(number) do
-  #
-  # end
-  #
-  # def loadFile(filename) do
-  #  {_result, device} = File.open(filename, [:read, :utf8])
-  #  preBuildListof1to100numberWordCountCombo(device, [], 1, &countLettersOfNumberAsWord/1)
-  # end
-  #
-  # def preBuildListof1to100numberWordCountCombo(file, count, lines, countFunc) do
-  #   line = IO.read(file, :line)
-  #   case line do
-  #     :eof ->
-  #       File.close(file)
-  #       lines
-  #     _ ->
-  #       l = lines ++ [{count, countFunc line}]
-  #   end
-  # end
+  def process(line) do
+    String.replace(line, "-", "") |> String.length
+  end
 
+  def readFile() do
+    listWithIndex = HelperLibrary.process_file_to_list!('numbers.dat', &process/1)
+    |> Enum.with_index
 
+    # add to dictionary, increment index + 1 and use as key (optional, else just remember its 0 based)
+
+    # todo
+
+    # 1..1000
+    # if less than 100
+    #   take from dictionary
+    # if gt or eq 100 and lt 1000
+    #   first digit (hundred) on right will be 1 to 9 + "AND"(3) + second two digits via dict look up
+    # if eq 1000
+      # 12
+    # add all together
+    # profit??
+
+  end
 end
