fddc6ab13e395bbd8f0bee82db34e2984fd9a964
Will <william.projects.2012@gmail.com>
2015-02-23 19:30:16 +0000

    Problem 17 at last (hooray for unit tests)

--- ProjectEuler/project_euler/lib/problem17.ex
+++ ProjectEuler/project_euler/lib/problem17.ex
@@ -1,26 +1,101 @@
 defmodule Problem17 do
+  
+  def solve() do
 
-  def process(line) do
-    String.replace(line, "-", "") |> String.length
-  end
+    file = open("F:\\Documents\\GitHub\\Elixir_Learn\\ProjectEuler\\project_euler\\lib\\numbers.dat")
 
-  def readFile() do
-    listWithIndex = HelperLibrary.process_file_to_list!('numbers.dat', &process/1)
-    |> Enum.with_index
+    listWithIndex = build_list file, [], 1
 
-    # add to dictionary, increment index + 1 and use as key (optional, else just remember its 0 based)
+    lookup = build_lookup_dictionary listWithIndex
 
-    # todo
+    result = Enum.reduce( 1..1000, 0, fn x, acc -> resolve lookup, x, acc end )  
 
-    # 1..1000
-    # if less than 100
-    #   take from dictionary
-    # if gt or eq 100 and lt 1000
-    #   first digit (hundred) on right will be 1 to 9 + "AND"(3) + second two digits via dict look up
-    # if eq 1000
-      # 12
-    # add all together
-    # profit??
+    result
 
   end
+
+  def resolve(lookup, x, acc) do
+
+    if (x < 100) do
+      acc = handleLessThan100 lookup, acc, x 
+    end
+
+    if (x >= 100) and (x < 1000) and (rem(x, 100) == 0) do # 100, 200 ...
+      acc = handleHundreds lookup, acc, x
+    end
+      
+    if x > 100 and x < 1000 and (rem(x, 100) != 0) do # 101, 202, 303 ...
+      acc = handleHundredsOther lookup, acc, x
+    end
+
+    if x == 1000 do
+      acc = acc + String.length("onethousand")
+    end
+
+    acc
+
+  end
+
+  def findLength(line) do
+    p1 = line |> String.strip |> String.replace "-", "" 
+    p1 = p1 |> String.replace " ", ""
+    p1 |> String.length
+  end
+
+  def build_lookup_dictionary list do
+    Enum.reduce(list, HashDict.new(), fn(x, dict) -> 
+      {k, v} = x
+      dict = HashDict.put(dict, k, v)
+      dict
+    end
+    )
+  end
+
+  def handleLessThan100(lookup, acc, x) do
+    {_, v} = HashDict.fetch(lookup, x)        
+    acc = acc + v
+    acc
+  end
+
+  def handleHundreds(lookup, acc, x) do
+    hundred = div(x, 100)
+    {_, h} = HashDict.fetch(lookup, hundred)
+    acc = acc + h + String.length("hundred")
+    acc
+  end
+
+  def handleHundredsOther(lookup, acc, x) do
+    hundred = div(x, 100)
+    ten = x - (hundred * 100)
+    {_, h} = HashDict.fetch(lookup, hundred)
+    {_, t} = HashDict.fetch(lookup, ten)
+    acc = acc + h + String.length("hundred") + String.length("and") + t
+    acc
+  end
+
+  def open(filename) do
+    {result, device} = File.open(filename, [:read, :utf8])
+    if result == :error do
+      IO.puts "error loading file"
+    else
+      device
+    end
+  end
+
+  def build_list(file, lines, linenumber) do
+    line = IO.read(file, :line)
+    case line do
+      :eof ->
+        File.close(file)
+        lines
+      _ ->
+        #IO.puts "#{line}"
+        length = findLength line
+        lines = [{linenumber, length}] ++ lines
+        build_list file, lines, linenumber + 1
+    end
+  end  
 end
+
+
+
--- ProjectEuler/project_euler/test/project_euler_test.exs
+++ ProjectEuler/project_euler/test/project_euler_test.exs
@@ -35,4 +35,59 @@ test "Test Higher order function" do
   assert result == 3
 end
 
+test "P17 lookup between 1 - 99" do
+  lookup = HashDict.new
+  lookup = HashDict.put(lookup, 1, 3)
+  lookup
+  num = Problem17.handleLessThan100(lookup, 0, 1) 
+  assert num == 3
+end
+
+test "P17 lookup hundred 100" do
+  sut = String.length "onehundred"
+  lookup = HashDict.new
+  lookup = HashDict.put(lookup, 1,  String.length "one")
+  assert Problem17.handleHundreds(lookup, 0, 100) == sut
+end
+
+test "P17 lookup between 101 - 199" do
+
+  sut = String.length "onehundredandninetynine"
+  lookup = HashDict.new
+  lookup = HashDict.put(lookup, 1,  String.length "one")
+  lookup = HashDict.put(lookup, 99,  String.length "ninetynine")
+  lookup
+  num = Problem17.handleHundredsOther(lookup, 0, 199) 
+  assert num == sut
+end
+
+test "P17 test process line and find length of line" do
+  len = Problem17.findLength "one-hun" 
+  assert len == 6
+end
+
+test "P17 test line extraction/cleanup and returned length" do
+  length = Problem17.findLength "one-hundred and ninety nine\n"
+
+  assert length == 23
+end
+
+test "P17 test building lookup dict from tuple list" do
+  list = [ {1, 10}, {2, 20}, {3, 30} ]
+  lookup = Problem17.build_lookup_dictionary list
+  {_, val} = HashDict.fetch(lookup, 2)
+  assert val == 20
+end
+
+test "File is loading correct number of lines to the list" do
+  file = Problem17.open("F:\\Documents\\GitHub\\Elixir_Learn\\ProjectEuler\\project_euler\\lib\\numbers.dat")
+  list = Problem17.build_list file, [], 1
+  assert length(list) == 100
+end
+
+test "Problem 17 Solve gives 21124" do
+  assert Problem17.solve == 21124
+end
+
+
 end
