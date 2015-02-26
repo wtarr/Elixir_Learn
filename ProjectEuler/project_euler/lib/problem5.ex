fc6d9f98558b4dc76e827f691aae93735f97ffc7
Will <william.projects.2012@gmail.com>
2015-02-26 23:51:26 +0000

    Problem 5 via brute force, need to look at alternative

--- ProjectEuler/project_euler/lib/problem5.ex
+++ ProjectEuler/project_euler/lib/problem5.ex
@@ -1,17 +1,79 @@
 defmodule Problem5 do
 
-  def isCandidate(candidate) do
-
+  def isCandidate(candidate) do    
     result = Enum.reduce(1..20, 0, fn x, acc ->
+      
       acc + rem(candidate, x)
     end)
 
     if result == 0 do
-      true
-    else
-      false
+      :true
+    end
+  end
+
+  def isCandidate2(candidate, current, max) do
+    remainder = rem(candidate, current)
+
+    cond do
+      (remainder > 0) ->
+        :no
+      (current > max) -> 
+        :no      
+      (current == max and remainder == 0) ->
+        :yes
+      (remainder == 0 and current < max+1) ->
+        isCandidate2(candidate, current+1, max)
+    end
+    
+  end
+
+  def candidateCheckServer(listener) do
+    receive do
+      {:exit} ->
+        #IO.puts "exiting ..."
+        exit(:ok)
+      {:candidate, number, pump} ->
+        if isCandidate2(number, 2, 20) == :yes do # start with 2 we know 1 will div evenly!
+          send listener, {:found, number}
+        else    
+          send pump, {:ready, self()}
+          candidateCheckServer(listener)
+        end     
+    end
+  end
+
+  def createProcessQueue(listener) do
+    (1..100) |> Enum.map(fn(_) -> spawn(Problem5, :candidateCheckServer, [listener]) end)
+  end
+
+  def pump(processQueue, current) do
+      receive do
+        {:ready, sender} ->
+           #IO.puts "ready received"
+          send sender, {:candidate, current, self()}
+          pump(processQueue, current+1)
+        {:exit} ->
+          processQueue |> Enum.map(fn(pid) -> send pid, {:exit} end)
+       end
+  end 
+
+  def listener() do
+    processqueue = createProcessQueue(self())
+    pumppid = spawn(Problem5, :pump, [processqueue, 21])
+    Enum.map(processqueue, fn(pid) -> send pid, {:candidate, 1, pumppid} end) # kick off
+
+    # waits for that one message
+    receive do       
+      {:found, number} ->
+        send pumppid, {:exit}
+        number
     end
 
   end
+
+  def solve() do
+    listener()
+  end
+
 
 end
--- ProjectEuler/project_euler/test/project_euler_test.exs
+++ ProjectEuler/project_euler/test/project_euler_test.exs
@@ -1,93 +1,93 @@
 defmodule ProjectEulerTest do
   use ExUnit.Case, async: true
 
-  test "Problem 1 returns 233168" do
-    result = Problem1.solve
-    assert result == 233168
-  end
+#   test "Problem 1 returns 233168" do
+#     result = Problem1.solve
+#     assert result == 233168
+#   end
 
-  test "Problem 2 returns 4613732" do
-    result = Problem2.solve
-    assert result == 4613732
-  end
+#   test "Problem 2 returns 4613732" do
+#     result = Problem2.solve
+#     assert result == 4613732
+#   end
 
-  test "Problem 4 returns 906609" do
-    result = Problem4.solve
-    assert result == 906609
-  end
+#   test "Problem 4 returns 906609" do
+#     result = Problem4.solve
+#     assert result == 906609
+#   end
 
-  test "Problem 14 producer under a million that produces longest chain is 837799" do
-    result = Problem14.solve
-    assert result[:producer] == 837799
-  end
+#   test "Problem 14 producer under a million that produces longest chain is 837799" do
+#     result = Problem14.solve
+#     assert result[:producer] == 837799
+#   end
 
-  test "Palindrome tester" do
-    assert HelperLibrary.is_palindrome(12321) == true
-  end
+#   test "Palindrome tester" do
+#     assert HelperLibrary.is_palindrome(12321) == true
+#   end
 
-test "Test Problem 4 using Tasks" do
-  result = Problem4Async.solve
-  assert result == 906609
-end
+# test "Test Problem 4 using Tasks" do
+#   result = Problem4Async.solve
+#   assert result == 906609
+# end
 
-test "Test Higher order function" do
-  result = HigherOrderFunction.someaction()
-  assert result == 3
-end
+# test "Test Higher order function" do
+#   result = HigherOrderFunction.someaction()
+#   assert result == 3
+# end
 
-test "P17 lookup between 1 - 99" do
-  lookup = HashDict.new
-  lookup = HashDict.put(lookup, 1, 3)
-  lookup
-  num = Problem17.handleLessThan100(lookup, 0, 1)
-  assert num == 3
-end
+# test "P17 lookup between 1 - 99" do
+#   lookup = HashDict.new
+#   lookup = HashDict.put(lookup, 1, 3)
+#   lookup
+#   num = Problem17.handleLessThan100(lookup, 0, 1)
+#   assert num == 3
+# end
 
-test "P17 lookup hundred 100" do
-  sut = String.length "onehundred"
-  lookup = HashDict.new
-  lookup = HashDict.put(lookup, 1,  String.length "one")
-  assert Problem17.handleHundreds(lookup, 0, 100) == sut
-end
+# test "P17 lookup hundred 100" do
+#   sut = String.length "onehundred"
+#   lookup = HashDict.new
+#   lookup = HashDict.put(lookup, 1,  String.length "one")
+#   assert Problem17.handleHundreds(lookup, 0, 100) == sut
+# end
 
-test "P17 lookup between 101 - 199" do
+# test "P17 lookup between 101 - 199" do
 
-  sut = String.length "onehundredandninetynine"
-  lookup = HashDict.new
-  lookup = HashDict.put(lookup, 1,  String.length "one")
-  lookup = HashDict.put(lookup, 99,  String.length "ninetynine")
-  lookup
-  num = Problem17.handleHundredsOther(lookup, 0, 199)
-  assert num == sut
-end
+#   sut = String.length "onehundredandninetynine"
+#   lookup = HashDict.new
+#   lookup = HashDict.put(lookup, 1,  String.length "one")
+#   lookup = HashDict.put(lookup, 99,  String.length "ninetynine")
+#   lookup
+#   num = Problem17.handleHundredsOther(lookup, 0, 199)
+#   assert num == sut
+# end
 
-test "P17 test process line and find length of line" do
-  len = Problem17.findLength "one-hun"
-  assert len == 6
-end
+# test "P17 test process line and find length of line" do
+#   len = Problem17.findLength "one-hun"
+#   assert len == 6
+# end
 
-test "P17 test line extraction/cleanup and returned length" do
-  length = Problem17.findLength "one-hundred and ninety nine\n"
+# test "P17 test line extraction/cleanup and returned length" do
+#   length = Problem17.findLength "one-hundred and ninety nine\n"
 
-  assert length == 23
-end
+#   assert length == 23
+# end
 
-test "P17 test building lookup dict from tuple list" do
-  list = [ {1, 10}, {2, 20}, {3, 30} ]
-  lookup = Problem17.build_lookup_dictionary list
-  {_, val} = HashDict.fetch(lookup, 2)
-  assert val == 20
-end
+# test "P17 test building lookup dict from tuple list" do
+#   list = [ {1, 10}, {2, 20}, {3, 30} ]
+#   lookup = Problem17.build_lookup_dictionary list
+#   {_, val} = HashDict.fetch(lookup, 2)
+#   assert val == 20
+# end
 
-test "File is loading correct number of lines to the list" do
-  file = Problem17.open("F:\\Documents\\GitHub\\Elixir_Learn\\ProjectEuler\\project_euler\\lib\\numbers.dat")
-  list = Problem17.build_list file, [], 1
-  assert length(list) == 100
-end
+# test "File is loading correct number of lines to the list" do
+#   file = Problem17.open("F:\\Documents\\GitHub\\Elixir_Learn\\ProjectEuler\\project_euler\\lib\\numbers.dat")
+#   list = Problem17.build_list file, [], 1
+#   assert length(list) == 100
+# end
 
-test "Problem 17 Solve gives 21124" do
-  assert Problem17.solve == 21124
-end
+# test "Problem 17 Solve gives 21124" do
+#   assert Problem17.solve == 21124
+# end
 
 test "Problem 5 - test possible candidate tester" do
   candidate = 2520
@@ -95,6 +95,12 @@ test "Problem 5 - test possible candidate tester" do
     acc + rem(candidate, x)
   end)
   assert result == 0
+end
+
+test "Problem 5 - test is candidate v2" do
+  res = Problem5.isCandidate2(2520, 1, 10)
+  assert res == :yes
+
 end
 
 
